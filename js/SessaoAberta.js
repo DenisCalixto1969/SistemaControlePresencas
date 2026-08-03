963eb0df215b5fb88d6ea413965c08a28445eba1
DenisCalixto1969 <denilsonclx@gmail.com>
2026-08-03 17:30:29 -0300

    abrir sessao em andamento

--- js/SessaoAberta.js
+++ js/SessaoAberta.js
@@ -1,7 +1,6 @@
 "use strict";
 
 async function carregarSessaoAberta(idSessao) {
-
     console.log("Abrindo sessão:", idSessao);
 
     const sessao = await buscarRegistroPorId(
@@ -10,5 +9,40 @@ async function carregarSessaoAberta(idSessao) {
     );
 
     console.log("Dados da sessão:", sessao);
+
+    const todasPresencas = await listarRegistros(
+        "presencas"
+    );
+
+    const presencas = todasPresencas.filter(
+        (presenca) => presenca.sessaoId === idSessao
+    );
+
+    const membrosDaSessao = await Promise.all(
+        presencas.map(async (presenca) => {
+            const membro = await buscarRegistroPorId(
+                "membros",
+                presenca.membroId
+            );
+
+            return {
+                presenca,
+                membro
+            };
+        })
+    );
+
+    console.log(
+        "Membros da sessão:",
+        membrosDaSessao
+    );
+
+    console.table(
+    membrosDaSessao.map(item => ({
+        nome: item.membro?.nome,
+        grau: item.membro?.grau,
+        presente: item.presenca.presente
+    }))
+);
 
 }
